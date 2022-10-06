adcce3688ec1ee28e6527cd3b15a217810a298bd
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-10-06 14:36:26 +0200

    feat: define model schema

--- src/models/restaurant.js
+++ src/models/restaurant.js
@@ -1,6 +1,25 @@
 const mongoose = require("mongoose");
 
-const restaurantSchema = new mongoose.Schema({});
+const restaurantSchema = new mongoose.Schema({
+  address: {
+    type: mongoose.Schema.Types.Mixed,
+  },
+  borough: {
+    type: mongoose.Schema.Types.String,
+  },
+  cuisine: {
+    type: mongoose.Schema.Types.String,
+  },
+  grades: {
+    type: [mongoose.Schema.Types.Mixed],
+  },
+  name: {
+    type: mongoose.Schema.Types.String,
+  },
+  restaurant_id: {
+    type: mongoose.Schema.Types.String,
+  },
+});
 
 const Restaurant = mongoose.model("restaurants", restaurantSchema);
 
