390e1eb1abd6684366e37791f2a81cc5d31b5b72
Ezequiel Dlr <erosarosario@cifpfbmoll.eu>
2022-10-06 14:36:09 +0200

    feat: add route to get restaurants by cuisine

--- src/routes/restaurants.js
+++ src/routes/restaurants.js
@@ -1,7 +1,8 @@
 const express = require("express");
-const { getAll } = require("../../controllers/restaurants");
+const { getAll, getAllByCuisine } = require("../controllers/restaurants");
 const router = express.Router();
 
 router.get("/", getAll);
+router.get("/cuisine/:cuisine", getAllByCuisine);
 
-module.exports = router
+module.exports = router;
